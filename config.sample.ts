795590067f817c410dcad31bac0af469e7e1b230
chikuwa111 <take0shun1111@gmail.com>
2019-10-14 11:31:49 +0900

    Change exec file structure

---
+++ config.sample.ts
@@ -0,0 +1,12 @@
+export type Config = {
+  chromeExecutablePath: string;
+  scrapboxProject: string;
+};
+
+const config: Config = {
+  chromeExecutablePath:
+    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  scrapboxProject: "xxxxxxx",
+};
+
+export default config;
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { Config } from "../config";
+
+export default async function main(config: Config) {
+  console.log(config);
+}
